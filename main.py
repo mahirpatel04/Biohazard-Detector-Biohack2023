42020a8604ad486b95014147a86da37d0306ac6d
Mahir Patel <126451249+mahirpatel04@users.noreply.github.com>
2023-05-21 02:20:43 -0700

    Final Touches

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import cvzone
 from cvzone.ClassificationModule import Classifier
 
-cap = cv2.VideoCapture(1)
+liveFeed = cv2.VideoCapture(1)
 classifier = Classifier('BiohazardDetectorApp/keras_model.h5', 'BiohazardDetectorApp/labels.txt')
 #imports waste pictures
 imgWasteList = []
@@ -20,19 +20,19 @@ pathList = os.listdir(pathFolderBins)
 for path in pathList:
     imgBinsList.append(cv2.imread(os.path.join(pathFolderBins, path), cv2.IMREAD_UNCHANGED))   
     
-print(pathList)
+
 
 classDictionary = {0:None,
-             1:0,
-             2:0,
-             3:1,
-             4:1}
+            1:0,
+            2:0,
+            3:1,
+            4:1}
 
 while True:
-    _, img = cap.read()
+    _, img = liveFeed.read()
     imgResize = cv2.resize(img, (300 , 300))
     
-    imgBackground = cv2.imread('BiohazardDetectorApp/Resources/Live Feed.png')
+    backGround = cv2.imread('BiohazardDetectorApp/Resources/Live Feed.png')
     
     
     prediction = classifier.getPrediction(img)  
@@ -41,12 +41,12 @@ while True:
     classID = prediction[1] 
     
     if classID !=0:
-        imgBackground = cvzone.overlayPNG(imgBackground, imgWasteList[classID-1], (800, 150))
-        imgBackground = cvzone.overlayPNG(imgBackground, imgBinsList[classDictionary[classID]], (800, 500))
+        backGround = cvzone.overlayPNG(backGround, imgWasteList[classID-1], (800, 150))
+        backGround = cvzone.overlayPNG(backGround, imgBinsList[classDictionary[classID]], (800, 500))
     
 
     
-    imgBackground[100:100+300, 75:75+300] = imgResize
+    backGround[100:100+300, 75:75+300] = imgResize
     
-    cv2.imshow("Output", imgBackground)
+    cv2.imshow("Output", backGround)
     cv2.waitKey(1)
