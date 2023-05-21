6da875b8b639c6affef1677ce73c2aac270ccb46
Mahir Patel <126451249+mahirpatel04@users.noreply.github.com>
2023-05-20 23:35:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,35 @@ import cv2
 import cvzone
 from cvzone.ClassificationModule import Classifier
 
-videoCapture = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(1)
 classifier = Classifier('BiohazardDetectorApp/keras_model.h5', 'BiohazardDetectorApp/labels.txt')
-
+#imports waste pictures
 imgWasteList = []
 pathFolderWaste = 'BiohazardDetectorApp/Resources/ClassificationImages'
 pathList = os.listdir(pathFolderWaste)
 
-for pathway in pathList:
-    imgWasteList.append(cv2.imread(os.path.join(pathFolderWaste, pathway), cv2.IMREAD_UNCHANGED))
-    
-    
-    
+for path in pathList:
+    imgWasteList.append(cv2.imread(os.path.join(pathFolderWaste, path), cv2.IMREAD_UNCHANGED))
+#imports bins   
+imgBinsList = []
+pathFolderBins = 'BiohazardDetectorApp/Resources/Hazardous Check'
+pathList = os.listdir(pathFolderBins)
 
+for path in pathList:
+    imgBinsList.append(cv2.imread(os.path.join(pathFolderBins, path), cv2.IMREAD_UNCHANGED))   
+    
+print(pathList)
+
+classDictionary = {0:None,
+             1:0,
+             2:1}
 
 while True:
-    _, img = videoCapture.read()
-    imgResize = cv2.resize(img, (300, 300))
+    _, img = cap.read()
+    imgResize = cv2.resize(img, (300 , 300))
     
     imgBackground = cv2.imread('BiohazardDetectorApp/Resources/Live Feed.png')
+    
     
     prediction = classifier.getPrediction(img)  
     print(prediction)
@@ -29,8 +39,8 @@ while True:
     classID = prediction[1] 
     
     if classID !=0:
-        imgBackground = cvzone.overlayPNG(imgBackground, imgWasteList[classID -1], (800, 100))
-        
+        imgBackground = cvzone.overlayPNG(imgBackground, imgWasteList[classID-1], (800, 200))
+        imgBackground = cvzone.overlayPNG(imgBackground, imgBinsList[classDictionary[classID]], (800, 500))
     
 
     
@@ -39,8 +49,3 @@ while True:
     # cv2.imshow("Image", img)
     cv2.imshow("Output", imgBackground)
     cv2.waitKey(1)
-    
-    
-        
-        
-print('we got it gang')
