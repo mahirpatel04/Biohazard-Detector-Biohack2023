248f460408bc104202e5d67f549ae73e0baa5a76
Mahir Patel <126451249+mahirpatel04@users.noreply.github.com>
2023-05-20 19:13:20 -0700

    Update main.py to add functionality for two different items

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
+import os
 import cv2
+import cvzone
 from cvzone.ClassificationModule import Classifier
 
-cap = cv2.VideoCapture(1)
+videoCapture = cv2.VideoCapture(1)
 classifier = Classifier('BiohazardDetectorApp/keras_model.h5', 'BiohazardDetectorApp/labels.txt')
 
-imgBackground = cv2.imread('BiohazardDetectorApp/Resources/Live Feed.png')
+imgWasteList = []
+pathFolderWaste = 'BiohazardDetectorApp/Resources/ClassificationImages'
+pathList = os.listdir(pathFolderWaste)
+
+for pathway in pathList:
+    imgWasteList.append(cv2.imread(os.path.join(pathFolderWaste, pathway), cv2.IMREAD_UNCHANGED))
+    
+    
+    
+
+
 while True:
-    _, img = cap.read()
+    _, img = videoCapture.read()
+    imgResize = cv2.resize(img, (300, 300))
+    
+    imgBackground = cv2.imread('BiohazardDetectorApp/Resources/Live Feed.png')
+    
     prediction = classifier.getPrediction(img)  
     print(prediction)
-    cv2.imshow("Image", img)
+    
+    classID = prediction[1] 
+    
+    if classID !=0:
+        imgBackground = cvzone.overlayPNG(imgBackground, imgWasteList[classID -1], (800, 100))
+        
+    
+
+    
+    imgBackground[100:100+300, 100:100+300] = imgResize
+    # Displays the images here:
+    # cv2.imshow("Image", img)
     cv2.imshow("Output", imgBackground)
     cv2.waitKey(1)
     
